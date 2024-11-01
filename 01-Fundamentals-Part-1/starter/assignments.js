d8746f472d9c97ee5c72636160a60f24577c5c18
julien14 <juxishimwe@gmail.com>
2024-11-01 21:02:24 +0200

    01-Part

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,104 @@
+/*
+// Assignment NumberOne.
+
+let country = "Rwanda";
+let continent = "Africa";
+let population = 14; 
+
+console.log(" Country : " +country+ "," +"\n", "Continent : " +continent+"," +"\n", "Population : " +population); 
+console.log(country);
+console.log(continent);
+console.log(population)
+
+//Assignment Data types
+
+// const isIsland = true;
+let language; 
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+// Assignment Let, Const, Var
+
+language = "i Kinyarwanda";
+
+const country = "Rwanda";
+const continent = "Africa";
+const isIsland = false; 
+isIsland = true; 
+
+console.log(isIsland);
+
+
+    //Assignment Basic Operators
+    let country = "Rwanda";
+    let continent = "Africa";
+    let population = 14;
+    language = "i Kinyarwanda";
+//Ass One
+let halfCountryPopulation = population / 2; 
+
+//Ass one
+/*
+console.log("Q1. if your country split in half, How many people would live in each half? : " + halfCountryPopulation)
+*/
+
+//Ass two
+/*
+population++; 
+console.log("Q2. Increase population of our country by 1 : " +population); 
+*/
+
+//Ass three
+/*
+let finland = 6; 
+const morePopulationCountry = population > finland;
+console.log("Q3. Does your country have more people than Finland : " + morePopulationCountry); 
+*/
+
+//Ass four
+
+/*
+let averagePopulation = 33; 
+const lessPeopleCountry = population < averagePopulation; 
+console.log("Q4. Does your country have less people than the average country : " + lessPeopleCountry);
+*/
+
+//Ass five
+/*
+console.log("Q5. "+country+ ' is in ' +continent+ ' and its ' +population+ ' million people speak ' +language);
+*/
+
+//Ass six
+
+/*
+let country = "Rwanda";
+let continent = "Africa";
+let population = 14;
+language = "i Kinyarwanda";
+
+const description = `${country} is in ${continent} and its ${population} million people speak ${language}`; 
+// console.log(description);
+
+// Ass seven if condition
+
+if(population > 33){
+    console.log(`${country}'s population is 22 million below average`);
+}else{
+    console.log(`${country}'s population is ${33 - population} million below average`);
+}
+*/
+
+// Ass eight Type Conversion and Coercion
+
+/*
+console.log('Predict the result of thes 5 operations without executing them: '); 
+
+console.log('9' - '5'); // -> 4
+console.log('19' - '13' + '17'); // -> '617'
+console.log('19' - '13' + 17); // -> 23
+console.log('123' < 57); // -> false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
+*/
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,71 @@
+
+let js = "amazing";
+console.log (40 + 8 + 23 -10); 
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+ let _newjonas_$matilda = 'JM'
+
+ let myFirstJob = "Coder";
+let myCurrentJob = "Teacher"; 
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
+
+
+true; 
+let javascriptIsFun = true; 
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 'Jonas');
+console.log(typeof 23);
+
+// change the value of variable 
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let varia = true;
+console.log("This is the data type of this variable: " +typeof varia)
+
+//undifined datatype
+
+let year; 
+console.log(year);
+console.log(typeof year)
+year = 1999; 
+console.log(year+ " type of : " +typeof year);
+
+//Error that exist in type of error!!
+
+console.log(typeof null); 
+
+
+//let, const, var
+
+let age = 30; 
+age = 21; 
+console.log("Age is changed to 21: " +age)
+
+const birthYear = 1999; 
+// birthYear = 2002; 
+console.log("Birth year can't change: " +birthYear); //we can't reassign const it is an immutable. this code will give an error. 
+
+var job = 'programmer'; 
+job = 'Coder'; 
+console.log("updated job : " +job)
+
+lastName = "Ishimwe" //This is bad idea we always have to decreare variables. 
+console.log('last name is : '+lastName);
+
