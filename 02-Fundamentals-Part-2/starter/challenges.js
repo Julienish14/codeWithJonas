352aeb4f567bedec354ee3486ba9615c8af7fe5d
julien14 <juxishimwe@gmail.com>
2024-11-02 14:34:37 +0200

    part 02 End - Challenges

--- 02-Fundamentals-Part-2/starter/challenges.js
+++ 02-Fundamentals-Part-2/starter/challenges.js
@@ -192,3 +192,31 @@ const calcTip = function (bill) {
 
 */
 
+
+/*
+  BONUS:
+
+Write a function calcAverage which takes an array called arr as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it if you feel like it:
+
+1. First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+
+2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+
+3. Call the function with the totals array.
+
+*/
+
+/*
+//Results
+
+function calcAverage (arr){
+    let sum = 0; 
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    return sum / arr.length; 
+}
+
+console.log(calcAverage(bills));
+
+*/
