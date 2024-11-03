412c222d39de1592034839597ced1dde6c7fefaa
julien14 <juxishimwe@gmail.com>
2024-11-03 18:43:06 +0200

    fn01 practice two

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -233,3 +233,15 @@ console.log('19' - '13' + 17); // -> ? 23
 console.log('123' < 57); // -> ? false
 console.log(5 + 6 + '4' + 9 - 4 - 2); // -> ? 1143
 
+const numNeighbours = Number(prompt('How many neighbour countries does your contry have?'));
+
+if(numNeighbours === 1){
+    console.log('Only 1 border!'); 
+}else if(numNeighbours > 1){
+    console.log('More than 1 border'); 
+}else{
+    console.log('No border'); 
+}
+
+console.log(typeof numNeighbours);
+
