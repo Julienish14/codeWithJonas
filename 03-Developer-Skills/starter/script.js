95c2adca6f351e0e29574530b8c4852f3942b440
julien14 <juxishimwe@gmail.com>
2024-11-04 16:08:32 +0200

    03 Dev skills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,9 +1,4 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = '23';
-if (x === 34) console.log('this number');
-
-const calcAge = birthYear => 2024 - birthYear;
-
-console.log(calcAge(2003));
+// Using Google, StackOverflow and MDN
