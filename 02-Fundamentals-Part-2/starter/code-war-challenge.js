e410daba342a0ec3b44c003f938db48064b2796c
julien14 <juxishimwe@gmail.com>
2024-11-07 23:41:31 +0200

    challenges Seven

--- 02-Fundamentals-Part-2/starter/code-war-challenge.js
+++ 02-Fundamentals-Part-2/starter/code-war-challenge.js
@@ -114,3 +114,25 @@ console.log(XOo('zpzpzpzpp'));
 console.log(XOo('zzoo'));
 
 */
+
+/*
+//CHALLENGE SEVEN: Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result. It should remove all values from list a, which are present in list b keeping their order.
+
+const arrDiffe = function (a, b) {
+  const result = [];
+  for (let i = 0; i < a.length; i++) {
+    if (!b.includes(a[i])) {
+      result.push(a[i]);
+    }
+  }
+  return result;
+};
+
+//Other simple way to do it like PRO 😎
+
+const arrDiff = (a, b) => {
+  return a.filter(i => !b.includes(i));
+};
+
+console.log(arrDiffe([2, 4, 4, 1, 5], [2, 4, 3]));
+*/
