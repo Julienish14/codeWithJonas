bf3d087e8684606d624856340646e68d85426272
julien14 <juxishimwe@gmail.com>
2024-11-08 00:27:13 +0200

    Project one - Guess Number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,8 +1,23 @@
 'use strict';
 
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) document.querySelector('.message').textContent = '⛔️ No number!';
+});
+
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 14;
+console.log(document.querySelector('.guess').value);
+*/
 
 //Me trying to work on it with console.
 /*
@@ -12,9 +27,6 @@ const gameGuessNumber = function (number) {
   let score = 20;
   let highScore = 0;
 
-  for (let i = score; i > 0; i--) {
-    // console.log('Score: ' + score);
-  }
   if (num === number) {
     console.log('🎉 Correct number');
     highScore += 17;
