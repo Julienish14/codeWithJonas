3caad1acb95dc76e9d1f46dea01bf05c25c118e1
julien14 <juxishimwe@gmail.com>
2024-11-08 12:26:28 +0200

    ProjectOne changes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,17 +1,8 @@
 'use strict';
-
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
-
-  if (!guess) document.querySelector('.message').textContent = '⛔️ No number!';
-});
-
 /*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 console.log(document.querySelector('.message').textContent);
-
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
@@ -19,10 +10,32 @@ document.querySelector('.guess').value = 14;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 0;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) document.querySelector('.message').textContent = '⛔️ No number!';
+  else if (guess === secretNumber)
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
+
 //Me trying to work on it with console.
 /*
 const gameGuessNumber = function (number) {
-  const num = Math.trunc(Math.random() * 6) + 1;
+  const num = Math.trunc(Math.random() * 20) + 1;
   console.log('Number: ' + num);
   let score = 20;
   let highScore = 0;
