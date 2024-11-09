8fcef53c30cc5a1bbfffcaea0010f6132861d68b
julien14 <juxishimwe@gmail.com>
2024-11-09 16:15:33 +0200

    06 manipulate classe removing hidden

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,3 +6,10 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
