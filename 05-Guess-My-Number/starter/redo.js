16fa8750081e2b82b54b87d6f54b8f4ac849d23c
julien14 <juxishimwe@gmail.com>
2024-11-09 23:48:48 +0200

    redo some challenges

--- 05-Guess-My-Number/starter/redo.js
+++ 05-Guess-My-Number/starter/redo.js
@@ -1,16 +1,19 @@
 'use strict';
 
-const guess = Number(document.querySelector('.guess').value);
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
+
 document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent =
       '🎉 Woow!! Congratulations!';
-  } else if (!guess === secretNumber) {
-    document.querySelector('.message').textContent =
-      guess > secretNumber ? '📈 To High!' : '📉 To Low!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 To High!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 To Low!';
   }
 });
