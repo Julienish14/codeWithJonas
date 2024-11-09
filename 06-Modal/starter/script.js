e3d7623abf5f75742d50a769df956428299be02f
julien14 <juxishimwe@gmail.com>
2024-11-09 17:00:17 +0200

    06 Add Keypress event ESC

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,7 +5,6 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
@@ -20,3 +19,11 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+//Keypress event
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
