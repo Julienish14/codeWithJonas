610ecf00c869acdc413a6c24cccd26a73539c875
julien14 <juxishimwe@gmail.com>
2024-11-09 16:32:40 +0200

    06 Make it more DRY, more readable

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -14,12 +14,17 @@ for (let i = 0; i < btnsOpenModal.length; i++)
     overlay.classList.remove('hidden');
   });
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+btnCloseModal.addEventListener('click', closeModal);
 
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+overlay.addEventListener(
+  'click',
+  closeModal
+  //     function () {
+  //   modal.classList.add('hidden');
+  //   overlay.classList.add('hidden');
+  // }
+);
