e81ec3c445b5a8ac9eae07b338760664054dfddc
julien14 <juxishimwe@gmail.com>
2024-11-10 13:02:07 +0200

    practice modal modal

--- 06-Modal/starter/redo.js
+++ 06-Modal/starter/redo.js
@@ -1,25 +1,12 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const openModal = document.querySelectorAll('.show-modal');
-const closeModal = document.querySelector('.close-modal');
+const btnMdl = document.querySelectorAll('.show-modal');
+const btnClsMdl = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
 
-const displayModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
-const closeMo = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-for (let i = 0; i < openModal.length; i++)
-  openModal[i].addEventListener('click', displayModal);
-
-closeModal.addEventListener('click', closeMo);
-overlay.addEventListener('click', closeMo);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.add('hidden')) closeMo();
-});
+for (let i = 0; i < btnMdl.length; i++)
+  btnMdl[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
