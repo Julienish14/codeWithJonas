2faa3de8ce29ea5a357247a7da68d270a0e42cb7
julien14 <juxishimwe@gmail.com>
2024-11-11 18:13:54 +0200

    pig game changes active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -11,12 +11,15 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 
@@ -32,8 +35,11 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; //CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //Switch to next player
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
