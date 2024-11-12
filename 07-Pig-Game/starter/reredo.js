944db51098dfe6fcc6f3695dfddb73c65af09a38
julien14 <juxishimwe@gmail.com>
2024-11-12 16:39:00 +0200

    pig game fix

--- 07-Pig-Game/starter/reredo.js
+++ 07-Pig-Game/starter/reredo.js
@@ -15,7 +15,44 @@ diceRo.classList.add('hidden');
 scoreOne.textContent = 0;
 scoreTwo.textContent = 0;
 
+let scores = [0, 0];
+let currentScore = 0;
+let currentPlayer = 0;
+let playing = true;
+
 buttonRoll.addEventListener('click', function () {
-  diceRo.classList.remove('hidden');
-  const diceK = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    diceRo.classList.remove('hidden');
+
+    //Roll dice with different random side.
+    const diceK = Math.trunc(Math.random() * 6) + 1;
+    diceRo.src = `dice-${diceK}.png`;
+    currentScore += diceK;
+
+    document.getElementById(`current--${currentPlayer}`).textContent =
+      currentScore;
+  }
+});
+
+buttonHold.addEventListener('click', function () {
+  if (playing) {
+    scores[currentPlayer] += currentScore;
+    document.getElementById(`score--${currentPlayer}`).textContent =
+      scores[currentPlayer];
+
+    if (scores[currentPlayer] >= 20) {
+      //Game over
+      playing = false;
+      document
+        .querySelector(`.player--${currentPlayer}`)
+        .classList.add('player--winner');
+    } else {
+      document.getElementById(`current--${currentPlayer}`).textContent = 0;
+      currentPlayer = currentPlayer === 0 ? 1 : 0;
+      currentScore = 0;
+
+      playerOne.classList.toggle('player--active');
+      playerTwo.classList.toggle('player--active');
+    }
+  }
 });
