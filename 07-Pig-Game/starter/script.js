bd10d773f1fd954506a6e5a442f7dfca0f105553
julien14 <juxishimwe@gmail.com>
2024-11-12 13:34:03 +0200

    pig game finish

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -9,21 +9,40 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const winner0El = document.querySelector('.winner--0');
+const winner1El = document.querySelector('.winner--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  winner0El.classList.add('hidden');
+  winner1El.classList.add('hidden');
+
+  //   document.querySelector(`.winner--${activePlayer}`).classList.remove('hidden');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -110,11 +129,6 @@ btnHold.addEventListener('click', function () {
 //   //   switchPlayer();
 // });
 
-btnNew.addEventListener('click', function () {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-});
+// Jonas Way
+
+btnNew.addEventListener('click', init);
