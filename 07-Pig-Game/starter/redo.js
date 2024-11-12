254e8dcc5d5e00447c043aaea80e7ca3e6b7dc6b
julien14 <juxishimwe@gmail.com>
2024-11-12 15:30:03 +0200

    pig game reset new game

--- 07-Pig-Game/starter/redo.js
+++ 07-Pig-Game/starter/redo.js
@@ -2,10 +2,12 @@
 
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
-// const current0El = document.getElementById('current--0');
-// const current1El = document.getElementById('current--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
+const winner0El = document.querySelector('.winner--0');
+const winner1El = document.querySelector('.winner--1');
 
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -23,6 +25,8 @@ let scores = [0, 0];
 let playing = true;
 
 diceEl.classList.add('hidden');
+
+const init = () => {};
 
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -75,4 +79,26 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener('click', function () {
+  scores = [0, 0];
+  activePlayer = 0;
+  currentScore = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player-active');
+  player1El.classList.remove('player--active');
+
+  winner0El.classList.add('hidden');
+  winner1El.classList.add('hidden');
 });
