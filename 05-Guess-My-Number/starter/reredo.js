8b75b61cd811e10484ed924a51de817f6843e7c2
julien14 <juxishimwe@gmail.com>
2024-11-15 17:56:49 +0200

    replicated

--- 05-Guess-My-Number/starter/reredo.js
+++ 05-Guess-My-Number/starter/reredo.js
@@ -1,13 +1,30 @@
 'use strict';
 
 const num = document.querySelector('.number');
-
-// num.textContent = numberToGuess;
-
+const msg = document.querySelector('.message');
 const checkBtn = document.querySelector('.check');
+const score = document.querySelector('.score');
+const hiSco = document.querySelector('.highscore');
+
+let numberToGuess = Math.trunc(Math.random() * 5) + 1;
+
+let scoreN = 20;
+
 checkBtn.addEventListener('click', function () {
-  let numberToGuess = Math.trunc(Math.random() * 5) + 1;
   const youGuess = Number(document.querySelector('.guess').value);
-  console.log(typeof youGuess);
-  console.log(Number(youGuess));
+  num.textContent = numberToGuess;
+  if (youGuess === numberToGuess) {
+    msg.textContent = '🎉 Congz!';
+    let highscore = 0;
+    highscore = scoreN;
+    console.log(highscore);
+  } else if (youGuess > numberToGuess) {
+    msg.textContent = 'too high';
+    scoreN--;
+    score.textContent = scoreN;
+  } else if (youGuess < numberToGuess) {
+    msg.textContent = 'too low';
+    scoreN--;
+    score.textContent = scoreN;
+  }
 });
