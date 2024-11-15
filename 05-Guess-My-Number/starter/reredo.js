ae24d8fda0e837a3f5902bc43cc05379b02c1c74
julien14 <juxishimwe@gmail.com>
2024-11-15 17:24:51 +0200

    replicate project

--- 05-Guess-My-Number/starter/reredo.js
+++ 05-Guess-My-Number/starter/reredo.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const num = document.querySelector('.number');
+
+// num.textContent = numberToGuess;
+
+const checkBtn = document.querySelector('.check');
+checkBtn.addEventListener('click', function () {
+  let numberToGuess = Math.trunc(Math.random() * 5) + 1;
+  const youGuess = Number(document.querySelector('.guess').value);
+  console.log(typeof youGuess);
+  console.log(Number(youGuess));
+});
