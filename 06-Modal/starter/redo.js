f5132b4f6224708923e8184c8924f34d76bfb86e
julien14 <juxishimwe@gmail.com>
2024-11-15 19:13:47 +0200

    work on modal

--- 06-Modal/starter/redo.js
+++ 06-Modal/starter/redo.js
@@ -1,9 +1,17 @@
 'use strict';
 
 const showModal = document.querySelectorAll('.show-modal');
-const 
+const closeModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
 
 for (let i = 0; i < showModal.length; i++)
   showModal[i].addEventListener('click', function () {
-    console.log(showModal[i].textContent);
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
+
+closeModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
