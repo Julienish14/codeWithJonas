5848f92b33ae68e472e1fd8a7a6eaa72d673e4ae
julien14 <juxishimwe@gmail.com>
2024-11-15 18:15:13 +0200

    reset game button done

--- 05-Guess-My-Number/starter/reredo.js
+++ 05-Guess-My-Number/starter/reredo.js
@@ -5,8 +5,9 @@ const msg = document.querySelector('.message');
 const checkBtn = document.querySelector('.check');
 const score = document.querySelector('.score');
 const hiSco = document.querySelector('.highscore');
+const resetGame = document.querySelector('.again');
 
-let numberToGuess = Math.trunc(Math.random() * 5) + 1;
+let numberToGuess = Math.trunc(Math.random() * 10) + 1;
 
 let scoreN = 20;
 let highscore = 0;
@@ -24,8 +25,17 @@ checkBtn.addEventListener('click', function () {
     }
   } else if (youGuess !== numberToGuess) {
     msg.textContent = `${youGuess > numberToGuess ? 'Too High' : 'Too Low!'}`;
-
     scoreN--;
     score.textContent = scoreN;
   }
+});
+
+resetGame.addEventListener('click', function () {
+  scoreN = 20;
+  highscore = 0;
+  msg.textContent = 'Start guessing...';
+  hiSco.textContent = highscore;
+  score.textContent = scoreN;
+  num.textContent = '?';
+  document.querySelector('.guess').value = '';
 });
