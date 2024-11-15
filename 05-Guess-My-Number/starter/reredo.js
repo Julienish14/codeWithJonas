0ead7487b628f9082825aba2dee999a75fd45741
julien14 <juxishimwe@gmail.com>
2024-11-15 18:27:12 +0200

    last part of game

--- 05-Guess-My-Number/starter/reredo.js
+++ 05-Guess-My-Number/starter/reredo.js
@@ -12,30 +12,45 @@ let numberToGuess = Math.trunc(Math.random() * 10) + 1;
 let scoreN = 20;
 let highscore = 0;
 
+const dispM = mesg => (msg.textContent = mesg);
+
 checkBtn.addEventListener('click', function () {
   const youGuess = Number(document.querySelector('.guess').value);
   if (!youGuess) {
-    msg.textContent = 'No number';
-  } else if (youGuess === numberToGuess) {
+    dispM('No number');
+  }
+
+  if (youGuess === numberToGuess) {
     num.textContent = numberToGuess;
-    msg.textContent = '🎉 Congz!';
+    dispM('🎉 Congz!');
+
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
     if (scoreN > highscore) {
       highscore = scoreN;
       hiSco.textContent = highscore;
     }
   } else if (youGuess !== numberToGuess) {
-    msg.textContent = `${youGuess > numberToGuess ? 'Too High' : 'Too Low!'}`;
-    scoreN--;
-    score.textContent = scoreN;
+    if (scoreN > 1) {
+      msg.textContent = `${youGuess > numberToGuess ? 'Too High' : 'Too Low!'}`;
+      scoreN--;
+      score.textContent = scoreN;
+    } else {
+      dispM('You got no Game G!');
+      score.textContent = 0;
+    }
   }
 });
 
 resetGame.addEventListener('click', function () {
   scoreN = 20;
-  highscore = 0;
-  msg.textContent = 'Start guessing...';
+  dispM('Start guessing...');
+
   hiSco.textContent = highscore;
   score.textContent = scoreN;
   num.textContent = '?';
   document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#333';
+  document.querySelector('.number').style.width = '15rem';
 });
