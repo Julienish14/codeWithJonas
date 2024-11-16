596e2f46a0d82095340799ff6d434a3891d2fc93
julien14 <juxishimwe@gmail.com>
2024-11-16 12:18:42 +0200

    Project 03 - select El & start game

--- 07-Pig-Game/starter/reredo.js
+++ 07-Pig-Game/starter/reredo.js
@@ -1,3 +1,20 @@
 'use strict';
 
-//Select btns
+//Select every element we might need.
+
+const resetGame = document.querySelector('.btn--new');
+const rollDice = document.querySelector('.btn--roll');
+const holdPoints = document.querySelector('.btn--hold');
+
+const scoreElOne = document.getElementById('score--0');
+const scoreElTwo = document.getElementById('score--1');
+const currOneScore = document.getElementById('current--0');
+const currTwoScore = document.getElementById('current--1');
+
+const diceImgEl = document.querySelector('.dice');
+
+diceImgEl.classList.add('hidden');
+
+let holdedScore = 0;
+scoreElOne.textContent = holdedScore;
+scoreElTwo.textContent = holdedScore;
