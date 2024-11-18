88cd74f86a3e6f3a96cf4f1d089fa04077a8a64a
julien14 <juxishimwe@gmail.com>
2024-11-18 22:43:47 +0200

    work on challenge four

--- 01-Fundamentals-Part-1/starter/redo.js
+++ 01-Fundamentals-Part-1/starter/redo.js
@@ -49,3 +49,10 @@ switch (marks) {
   default:
     console.log('NOT QUALIFIED');
 }
+
+const bill = 430;
+let tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${tip + bill}`
+);
