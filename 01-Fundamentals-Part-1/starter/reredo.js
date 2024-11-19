1c2a87d7f3253c0e7efbbe56c0299d9c45c90bbe
julien14 <juxishimwe@gmail.com>
2024-11-19 21:24:57 +0200

    Redo some challenges one

--- 01-Fundamentals-Part-1/starter/reredo.js
+++ 01-Fundamentals-Part-1/starter/reredo.js
@@ -6,3 +6,8 @@ const heightJohn = 1.76;
 const BMIMark = massMark / (heightMark * heightMark);
 const BMIJohn = massJohn / (heightJohn * heightJohn);
 let markHigherBMI = BMIMark > BMIJohn;
+
+console.log('Body Mass Index of Mark: ' + BMIMark);
+console.log('Body Mass Index of John: ' + BMIJohn);
+
+console.log('Mark has hight BMI than John? : ' + markHigherBMI);
