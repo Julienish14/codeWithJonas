a76637ab59fb4bbda357467194e519f3fcb775c2
julien14 <juxishimwe@gmail.com>
2024-11-22 16:02:01 +0200

    scope chain

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -2,10 +2,34 @@
 
 function calcAge(birthYear) {
   const age = 2025 - birthYear;
-  console.log(firstName);
+
+  // console.log(firstName); //here what happens is called variable lookup, the variable was not in this scope, it loops up in scope chain and find it in parent scope
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true; //Var is function scope not like let&const(ES6)
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str); //is block scope
+    console.log(millenial);
+    //console.log(add(4, 5)); //bcz we use strict we can't use function from block
+  }
+
+  printAge();
+
   return age;
 }
 
 const firstName = 'Jonas';
-const a = calcAge(1991);
-console.log(a);
+console.log(calcAge(1991));
+// console.log(age);
+// printAge();
