3d56c69b3a878ddcf832c9723724f8b74657ecbb
julien14 <juxishimwe@gmail.com>
2024-11-22 15:32:04 +0200

    start code practice Scope

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2025 - birthYear;
+  console.log(firstName);
+  return age;
+}
+
+const firstName = 'Jonas';
+const a = calcAge(1991);
+console.log(a);
