4a5b516f064df91837be549771f9817d6e35f30b
julien14 <juxishimwe@gmail.com>
2024-11-28 21:41:13 +0200

    Work on FN challenge Two

---
+++ 02-Fundamentals-Part-2/starter/re-challenge.js
@@ -0,0 +1,14 @@
+'use strict';
+/* 
+CHALLENGE #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+
+A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+
+
+
+Your tasks:
+
+*/
