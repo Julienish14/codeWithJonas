d6f3ad8f3116599b5a2783b12159a443d1b7371f
julien14 <juxishimwe@gmail.com>
2024-11-28 21:42:49 +0200

    Challenge One on Part Two

--- 02-Fundamentals-Part-2/starter/re-challenge.js
+++ 02-Fundamentals-Part-2/starter/re-challenge.js
@@ -11,4 +11,9 @@ A team only wins if it has at least double the average score of the other team. 
 
 Your tasks:
 
+Create an arrow function calcAverage to calculate the average of 3 scores. This function should have three parameters and return a single number (the average score).
+
+Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the calcAverage function to them (you will need to call this function, and pass scores as arguments).
+
+Create a function checkWinner that takes the average score of each team as parameters (avgDolphins and avgKoalas), and then logs the winner to the console, together with the victory points, according to the rule above. Example: Koalas win (30 vs. 13) (use avgDolphins and avgKoalas instead of hard-coded values).
 */
