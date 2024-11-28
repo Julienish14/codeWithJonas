63a9088cef877e9ef62f7944a6991a58aab69c90
julien14 <juxishimwe@gmail.com>
2024-11-28 22:26:54 +0200

    Challenge Two fixed

--- 02-Fundamentals-Part-2/starter/re-challenge.js
+++ 02-Fundamentals-Part-2/starter/re-challenge.js
@@ -57,9 +57,10 @@ BONUS: Create an array totals containing the total values, so the bill + tip.
 TEST DATA: 125, 555, and 44.
 
 */
-let tip;
+
 const calTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? (tip = bill * 0.15) : (tip = bill * 20);
+  let tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+  return tip;
 };
 
 console.log(`Bill and Tip ${calTip(400)}`);
