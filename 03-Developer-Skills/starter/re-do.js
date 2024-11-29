ec99515d5b7593d8a7e3f799c0797d907572cebd
julien14 <juxishimwe@gmail.com>
2024-11-29 20:57:43 +0200

    Part 03 Developer skills

--- 03-Developer-Skills/starter/re-do.js
+++ 03-Developer-Skills/starter/re-do.js
@@ -4,6 +4,9 @@
 // PROBLEM 1:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
+1. How to not use(skip) error?
+2. How to calculate amplitude. maxTemp - minTemp
+3. How to cal Max & Min element in array.
 */
 const tempAmplitude = function (temp1, temp2) {
   const temp = temp1.concat(temp2);
@@ -20,3 +23,8 @@ const tempAmplitude = function (temp1, temp2) {
   console.log(max, min);
   return max - min;
 };
+
+const tempRoom1 = [23, 12, 44, 12];
+const tempRoom2 = ['Error', 3, 'Error'];
+const amplitude = tempAmplitude(tempRoom1, tempRoom2);
+console.log(amplitude);
