cae4f6427b37e86722bde1f688a293a7f1c27841
julien14 <juxishimwe@gmail.com>
2024-11-29 20:54:30 +0200

    Part 03 Developer skills

--- 03-Developer-Skills/starter/re-do.js
+++ 03-Developer-Skills/starter/re-do.js
@@ -5,3 +5,18 @@
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
 */
+const tempAmplitude = function (temp1, temp2) {
+  const temp = temp1.concat(temp2);
+  console.log(temp);
+  let max = temp[0];
+  let min = temp[0];
+  for (let i = 0; i < temp.length; i++) {
+    let currentTemp = temp[i];
+    if (typeof currentTemp !== 'number') continue;
+
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
