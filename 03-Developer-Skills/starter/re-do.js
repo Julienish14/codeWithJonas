09ab0d5093df5617aa16be08a6a3f0d978cc956c
julien14 <juxishimwe@gmail.com>
2024-12-01 17:43:30 +0200

    Fix one of the dev skills

--- 03-Developer-Skills/starter/re-do.js
+++ 03-Developer-Skills/starter/re-do.js
@@ -214,3 +214,16 @@ function isIsogram(str) {
   return true;
 }
 console.log('This is the ans: ' + isIsogram('backgrouND'));
+
+//CHALLENGE ONE: You are given a number say true or false if number is narcissistic ex: 153 -> 1^3 + 5^3 + 3^3
+
+function isItNarcissistic(num) {
+  const toStr = num.toString();
+  const numLeng = toStr.length;
+  let result = 0;
+  for (let i = 0; i < numLeng; i++) {
+    const y = parseInt(toStr[i]);
+    result += Math.pow(y, numLeng);
+  }
+  return result === num;
+}
