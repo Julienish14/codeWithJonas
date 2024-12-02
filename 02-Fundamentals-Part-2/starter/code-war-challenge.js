1b800da3ceae3338b9631ecc889d364a71845b6a
julien14 <juxishimwe@gmail.com>
2024-12-02 23:58:30 +0200

    Change some one codeWar challenge

--- 02-Fundamentals-Part-2/starter/code-war-challenge.js
+++ 02-Fundamentals-Part-2/starter/code-war-challenge.js
@@ -4,7 +4,20 @@
 
 //CHALLENGE ONE: You are given a number say true or false if number is narcissistic ex: 153 -> 1^3 + 5^3 + 3^3
 
+/*
+function isItNarcissistic(num) {
+  const toStr = num.toString();
+  const numLeng = toStr.length;
+  let result = 0;
+  for (let i = 0; i < numLeng; i++) {
+    const y = parseInt(toStr[i]);
+    result += Math.pow(y, numLeng);
+  }
+  return result === num;
+}
 
+console.log(isItNarcissistic(153));
+console.log(isItNarcissistic(13));
 
 //CHALLENGE TWO: Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
 
@@ -14,19 +27,6 @@ function repeater(n, s) {
     rep += s;
   }
   return rep;
-  function isItNarcissistic(num) {
-    const toStr = num.toString();
-    const numLeng = toStr.length;
-    let result = 0;
-    for (let i = 0; i < numLeng; i++) {
-      const y = parseInt(toStr[i]);
-      result += Math.pow(y, numLeng);
-    }
-    return result === num;
-  }
-  
-  console.log(isItNarcissistic(153));
-  console.log(isItNarcissistic(13));
 }
 console.log(repeater(4, 'HelloWorld|'));
 */
