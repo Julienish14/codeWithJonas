2a3bb231f4f623ab5e236c8f46217613c7452d1b
julien14 <juxishimwe@gmail.com>
2024-12-06 23:13:05 +0200

    modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -14,22 +14,6 @@ const openModal = function () {
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
