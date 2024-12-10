eb62589d62df8c468911f58a13e305c0ace44437
julien14 <juxishimwe@gmail.com>
2024-12-10 23:58:05 +0200

    Change

--- 02-Fundamentals-Part-2/starter/redo.js
+++ 02-Fundamentals-Part-2/starter/redo.js
@@ -2,6 +2,17 @@
 
 //CHALLENGE ONE: You are given a number say true or false if number is narcissistic ex: 153 -> 1^3 + 5^3 + 3^3
 
+function isItNarcissistic(num) {
+  const toStr = num.toString();
+  const numLeng = toStr.length;
+  let result = 0;
+  for (let i = 0; i < numLeng; i++) {
+    const y = parseInt(toStr[i]);
+    result += Math.pow(y, numLeng);
+  }
+  return result === num;
+}
+
 console.log(isItNarcissistic(153));
 console.log(isItNarcissistic(13));
 
@@ -15,14 +26,3 @@ function repeater(n, s) {
 console.log(repeater(4, 'HelloWorld|'));
 
 //This is just rewinding
-
-function isItNarcissistic(num) {
-  const toStr = num.toString();
-  const numLeng = toStr.length;
-  let result = 0;
-  for (let i = 0; i < numLeng; i++) {
-    const y = parseInt(toStr[i]);
-    result += Math.pow(y, numLeng);
-  }
-  return result === num;
-}
