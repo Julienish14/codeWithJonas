046ba00a4e7ec9b7e468440cdd02cce933f3c481
julien14 <juxishimwe@gmail.com>
2024-12-21 14:01:14 +0200

    this keyword in practice

--- 08-Behind-the-Scenes/starter/redo.js
+++ 08-Behind-the-Scenes/starter/redo.js
@@ -84,16 +84,32 @@ console.log(z === window.z);
 
 //This key word in action.
 
-console.log(this);
+// console.log(this);
 
 const calcAge = function (birthYear) {
   console.log(2024 - birthYear);
-  console.log(this);
+  //   console.log(this);
 };
 calcAge(1991);
 
 const calcAgeArrow = birthYear => {
   console.log(2024 - birthYear);
-  console.log(this);
+  //   console.log(this);
 };
 calcAgeArrow(1991);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2024 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
