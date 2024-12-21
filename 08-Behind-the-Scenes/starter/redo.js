8378a8f60a6780e8a4753b79b0543517269183c9
julien14 <juxishimwe@gmail.com>
2024-12-21 02:35:00 +0200

    Scoping works

--- 08-Behind-the-Scenes/starter/redo.js
+++ 08-Behind-the-Scenes/starter/redo.js
@@ -4,12 +4,18 @@ function calcAge(birthYear) {
   const age = 2024 - birthYear;
 
   function printAge() {
-    const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
       var millenial = true;
+
+      //Creating new variable as same as outer scope's variable
       const firstName = 'Steven';
+
+      //Reassigning outer scope's variable
+      output = `NEW OUTPUT!`;
+
       const str = `Oh, and you're a millenial, ${firstName}`;
       console.log(str);
 
@@ -20,6 +26,7 @@ function calcAge(birthYear) {
     // console.log(str);
     console.log(millenial);
     // console.log(add(3, 4));
+    console.log(output);
   }
   printAge();
 
