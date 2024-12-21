57717577809ce8577307caf367f01637a26a696f
julien14 <juxishimwe@gmail.com>
2024-12-21 02:22:27 +0200

    Scoping chain

--- 08-Behind-the-Scenes/starter/redo.js
+++ 08-Behind-the-Scenes/starter/redo.js
@@ -4,10 +4,25 @@ function calcAge(birthYear) {
   const age = 2024 - birthYear;
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
+    const output = `${firstName}, You are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(3, 4));
   }
   printAge();
+
   return age;
 }
 
