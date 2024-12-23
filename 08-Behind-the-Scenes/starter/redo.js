0c601b0d862c0ff024d22505339654fe887f5e0d
julien14 <juxishimwe@gmail.com>
2024-12-24 00:13:32 +0200

    JS Behind Argument keyword

--- 08-Behind-the-Scenes/starter/redo.js
+++ 08-Behind-the-Scenes/starter/redo.js
@@ -160,3 +160,9 @@ const addExpr = function (a, b) {
 };
 addExpr(2, 5);
 addExpr(2, 5, 6, 71);
+
+// var addArrow = (a, b) => {
+//   console.log(arguments);
+//   return a + b;
+// }; //argument function doesn't exist in arrow fun
+// addArrow(2, 5, 6);
