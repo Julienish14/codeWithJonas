245c9be9ec22cc6443f2e5955536f3e643b9718a
julien14 <juxishimwe@gmail.com>
2024-12-28 20:12:37 +0200

    Be JS

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -122,3 +122,46 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 14;
 console.log(document.querySelector('.guess').value);
+
+et secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //When there is no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔️ No number!';
+    displayMessage('⛔️ No number!');
+
+    //When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    // document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    // REFACTORING: DRY --- When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? '📈Too high!' : '📉Too low!';
+      displayMessage(guess > secretNumber ? '📈Too high!' : '📉Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥You lost the game');
+      // document.querySelector('.message').textContent = '💥You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
