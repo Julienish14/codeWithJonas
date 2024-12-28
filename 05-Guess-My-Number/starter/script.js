501363603f870aa9573979096d9cdd91b275c429
julien14 <juxishimwe@gmail.com>
2024-12-28 20:11:34 +0200

    JS

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -113,3 +113,12 @@ document.querySelector('.again').addEventListener('click', function () {
 });
 
 */
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 14;
+console.log(document.querySelector('.guess').value);
