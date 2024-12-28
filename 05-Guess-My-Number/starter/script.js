45767ddfd58bb46d974555cb7818ccc2c79a039e
julien14 <juxishimwe@gmail.com>
2024-12-28 20:13:26 +0200

    Be JS

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -165,3 +165,18 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+
+  document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+  
+    // document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?';
+  
+    //CSS or Styles manipulation
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+  });
