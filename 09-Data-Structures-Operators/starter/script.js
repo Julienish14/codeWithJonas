1912de0ca7bb73467301e79bd3c10f1e689b5877
julien14 <juxishimwe@gmail.com>
2025-01-10 11:07:06 +0200

    De-structuring Array by function, Nested, Default value

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -49,3 +53,21 @@ console.log(main, secondary);
 
 [main, secondary] = [secondary, main]; //using destructuring
 console.log(main, secondary);
+
+// console.log(restaurant.order(2, 0));
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// De-structuring nested array
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default values
+const [p = 1, q = 1, r = 1] = [8];
+console.log(p, q, r);
