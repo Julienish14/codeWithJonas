d15c026f440c4879f4fd66ebb8d6a84621d2f115
julien14 <juxishimwe@gmail.com>
2025-01-10 11:25:16 +0200

    Assignment about destructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -31,7 +31,7 @@ const restaurant = {
     },
   },
 };
-
+/*
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -68,6 +68,6 @@ const nested = [2, 4, [5, 6]];
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
 
-//Default values
 const [p = 1, q = 1, r = 1] = [8];
 console.log(p, q, r);
+*/
