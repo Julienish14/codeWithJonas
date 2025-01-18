f2ccafe5ddcd0d3a0f8bc6b026bd6f2c4e8e8d73
julien14 <juxishimwe@gmail.com>
2025-01-18 21:19:52 +0200

    Short circuiting && and ||

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -57,6 +57,24 @@ const restaurant = {
   },
 };
 
+////////////////////////////////////////
+//Short circuiting (&& and ||)
+
+// use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guest1);
+
+const guest2 = restaurant.numGuests || 10;
+console.log(guest2);
+
 /*
 /////////////////////////////////////
 // Rest pattern and parameters
