25726b5cda6ccfb0dc8f58d6b8c39c74f69b0d22
julien14 <juxishimwe@gmail.com>
2025-01-19 19:06:13 +0200

    Work Assignment on Short circuiting && and ||

--- 09-Data-Structures-Operators/starter/assignment.js
+++ 09-Data-Structures-Operators/starter/assignment.js
@@ -372,3 +372,24 @@ const printBookAuthorsCount = function (title, ...author) {
 printBookAuthorsCount('CoverEyes', 'Kevin', 'Kon', 'Bob');
 
 */
+
+//5.1
+//Some of the book objects have the programmingLanguage property, which specifies what programming language is used in the book, for example
+/*
+{
+  title: 'Algorithms',
+  author: ['Robert Sedgewick', 'Kevin Wayne'],
+  ...
+  programmingLanguage: 'Java',     // <-- HERE
+}
+  
+Write a function called hasExamplesInJava that takes a book object from the books array as an argument. This function should return true if the book uses Java, or a string 'no data available' if it uses other language or no programming language at all.
+
+Use short-circuiting.
+*/
+
+function hasExamplesInJava(book) {
+  return book.programmingLanguage === 'Java' || 'no data available';
+}
+
+console.log(hasExamplesInJava(books[0]));
