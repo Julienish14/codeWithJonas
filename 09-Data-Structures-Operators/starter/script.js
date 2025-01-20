014f6ff9666ea639824f444f2c97d91c71902b19
julien14 <juxishimwe@gmail.com>
2025-01-20 12:04:27 +0200

    the nullish coalescing operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -58,9 +58,13 @@ const restaurant = {
 //////////////////////////////////////////////////
 //The Nullish coalescing operator (??)
 
-restaurant.numGuests = 0;
+// restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
 
 /*
 console.log('-------- OR --------');
