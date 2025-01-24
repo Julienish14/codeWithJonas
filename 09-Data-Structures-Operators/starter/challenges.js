94c74d7579a8bbbfce22d067996c2496b5ab412b
julien14 <juxishimwe@gmail.com>
2025-01-24 12:48:34 +0200

    challenges done

--- 09-Data-Structures-Operators/starter/challenges.js
+++ 09-Data-Structures-Operators/starter/challenges.js
@@ -8,11 +8,11 @@ Suppose we get data from a web service about a certain game ('game' variable on 
 Your tasks:
 1. Create one player array for each team(variables 'players1' and 'players2')
 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
-3. Createanarray'allPlayers'containingallplayersofbothteams(22 players)
+3. Create an array'allPlayers'containing all players of both teams(22 players)
 4. Duringthegame,BayernMunich(team1)used3substituteplayers.Socreatea new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-5. Basedonthegame.oddsobject,createonevariableforeachodd(called 'team1', 'draw' and 'team2')
-6. Writeafunction('printGoals')thatreceivesanarbitrarynumberofplayer names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-7. Theteamwiththeloweroddismorelikelytowin.Printtotheconsolewhich team is more likely to win, without using an if/else statement or the ternary operator.
+5. Based on the game.odds object,create one variable for each odd(called 'team1', 'draw' and 'team2')
+6. Write a function('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely towin. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
 Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 GOOD LUCK😀
 
@@ -65,12 +65,42 @@ console.log(players1, player2);
 
 //Ans 2
 const [gk, ...fieldPlayers] = game.players[0];
-console.log(gk, fieldPlayers);
+
+//Jonas Ans
+const [gk1, ...fieldPlayers1] = players1;
+console.log(gk1, fieldPlayers1);
 
 //Ans 3
 const allPlayers = [...game.players[0], ...game.players[1]];
+
+//Jonas Ans
+const allPlayerss = [...players1, ...player2];
 console.log(allPlayers);
 
 //Ans 4
-const players1Final = [...game.players[0], 'Thiago', 'Coutinho', 'Perisic'];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 console.log(players1Final);
+
+//Ans 5
+// const { team1, draw, team2 } = game.odds; //Failed
+
+//Jonas Ans
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+//Ans 6
+function printGoals(...players) {
+  console.log(`Players names: ${players}`);
+  console.log(`${players.length} goals were scored`);
+}
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals('Lewandowski', 'Kimmich');
+
+printGoals(...game.scored);
+
+//Ans 7
+team1 < team2 && console.log('Team one is more likely to win');
+team1 > team2 && console.log('Team Two is more likely to win');
