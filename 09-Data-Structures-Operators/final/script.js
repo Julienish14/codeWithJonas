3bb7c0544cba2afb49581a6dabb05ad41c59340c
julien14 <juxishimwe@gmail.com>
2025-01-29 23:42:56 +0200

    commit three

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -889,7 +889,14 @@ const c = arr[2];
 const [x, y, z] = arr;
 console.log(x, y, z);
 console.log(arr);
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
 
+// Switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 
@@ -905,26 +912,6 @@ console.log(main, secondary);
 // Receive 2 return values from a function
 const [starter, mainCourse] = restaurant.order(2, 0);
 console.log(starter, mainCourse);
-// Nested destructuring
-const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested;
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
-
-// Default values
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
-*/
-// Nested destructuring
-const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested;
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
-
-// Default values
-const [p = 1, q = 1, r = 1] = [8, 9];
-console.log(p, q, r);
-*/
 
 // Nested destructuring
 const nested = [2, 4, [5, 6]];
