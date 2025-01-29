4eb27ef93517ba24cda9230d9e8ced9f688a1213
julien14 <juxishimwe@gmail.com>
2025-01-29 23:45:43 +0200

    commit Six

--- 09-Data-Structures-Operators/starter/challenges.js
+++ 09-Data-Structures-Operators/starter/challenges.js
@@ -104,3 +104,18 @@ printGoals(...game.scored);
 //Ans 7
 team1 < team2 && console.log('Team one is more likely to win');
 team1 > team2 && console.log('Team Two is more likely to win');
+
+//Ans 6
+function printGoals(...players) {
+  console.log(`Players names: ${players}`);
+  console.log(`${players.length} goals were scored`);
+}
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals('Lewandowski', 'Kimmich');
+
+printGoals(...game.scored);
+
+//Ans 7
+team1 < team2 && console.log('Team one is more likely to win');
+team1 > team2 && console.log('Team Two is more likely to win');
