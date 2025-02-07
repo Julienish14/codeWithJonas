6ad65bf564e1b0035960d98da6593476e35198c2
julien14 <juxishimwe@gmail.com>
2025-02-07 17:08:59 +0200

    Reviewing some of operation operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -132,7 +132,7 @@ console.log(rest2);
 
 //////////////////////////////////////////////////
 //The Nullish Coalescing Operator (??)
-
+/*
 restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
 console.log(guests);
@@ -140,10 +140,7 @@ console.log(guests);
 // Nullish: null and undefined (NOT 0 or '')
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
-
-restaurant.owner = 0;
-const isOwner = restaurant.owner || 23;
-console.log(isOwner);
+*/
 
 /*
 console.log('-------- OR --------');
