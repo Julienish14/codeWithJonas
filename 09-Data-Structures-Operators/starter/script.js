11400923e6c3137bba56659430c8e5c445670b44
julien14 <juxishimwe@gmail.com>
2025-02-17 22:09:03 +0200

    looping object

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -418,27 +418,3 @@ console.log(p, q, r);
 */
 
 
-
-// De-structuring nested array
-const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested;
-// console.log(i, j);
-
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
-
-const [p = 1, q = 1, r = 1] = [8];
-console.log(p, q, r);
-*/
-
-// De-structuring nested array
-const nested = [2, 4, [5, 6]];
-// const [i, , j] = nested;
-// console.log(i, j);
-
-const [i, , [j, k]] = nested;
-console.log(i, j, k);
-
-const [p = 1, q = 1, r = 1] = [8];
-console.log(p, q, r);
-*/
