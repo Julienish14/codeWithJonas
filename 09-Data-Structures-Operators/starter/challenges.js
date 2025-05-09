365cb6fce4040b9b6c8521cd89e8c89c8aec14d8
julien14 <juxishimwe@gmail.com>
2025-05-10 00:20:02 +0200

    game

--- 09-Data-Structures-Operators/starter/challenges.js
+++ 09-Data-Structures-Operators/starter/challenges.js
@@ -101,9 +101,7 @@ printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
 printGoals('Lewandowski', 'Kimmich');
 
 printGoals(...game.scored);
-
+*/
 //Ans 7
 team1 < team2 && console.log('Team one is more likely to win');
 team1 > team2 && console.log('Team Two is more likely to win');
-
-*/
