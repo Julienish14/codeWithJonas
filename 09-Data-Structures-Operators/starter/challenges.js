4d79a10a7e5c046f5fc0bed86b0b4d8e8afffe33
julien14 <juxishimwe@gmail.com>
2025-05-14 15:42:00 +0200

    using vs code copilot

--- 09-Data-Structures-Operators/starter/challenges.js
+++ 09-Data-Structures-Operators/starter/challenges.js
@@ -59,7 +59,6 @@ const game = {
   },
 };
 
-/*
 //Ans 1
 const [players1, player2] = game.players;
 console.log(players1, player2);
@@ -105,5 +104,3 @@ printGoals(...game.scored);
 //Ans 7
 team1 < team2 && console.log('Team one is more likely to win');
 team1 > team2 && console.log('Team Two is more likely to win');
-
-*/
