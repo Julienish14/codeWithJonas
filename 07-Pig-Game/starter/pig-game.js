541ebf18dd5d529c2e9b26a4b4e5db9464b1e449
julien14 <juxishimwe@gmail.com>
2025-05-19 22:43:32 +0200

    Game redo

--- 07-Pig-Game/starter/pig-game.js
+++ 07-Pig-Game/starter/pig-game.js
@@ -64,26 +64,26 @@
 //   }
 // });
 
-// btn_hold.addEventListener('click', function () {
-//   if (isPlaying) {
-//     holdedScore[currentP] += currentScore;
-//     document.getElementById(`score--${currentP}`).textContent =
-//       holdedScore[currentP];
+btn_hold.addEventListener('click', function () {
+  if (isPlaying) {
+    holdedScore[currentP] += currentScore;
+    document.getElementById(`score--${currentP}`).textContent =
+      holdedScore[currentP];
 
-//     if (holdedScore[currentP] >= 100) {
-//       isPlaying = false;
-//       dice.classList.remove('hidden');
-//       document
-//         .querySelector(`.player--${currentP}`)
-//         .classList.add('player--winner');
-//       document.querySelector(`.winner--${currentP}`).classList.remove('hidden');
-//       document
-//         .querySelector(`.player--${currentP}`)
-//         .classList.remove('player--active');
-//     } else {
-//       swithGame();
-//     }
-//   }
-// });
+    if (holdedScore[currentP] >= 100) {
+      isPlaying = false;
+      dice.classList.remove('hidden');
+      document
+        .querySelector(`.player--${currentP}`)
+        .classList.add('player--winner');
+      document.querySelector(`.winner--${currentP}`).classList.remove('hidden');
+      document
+        .querySelector(`.player--${currentP}`)
+        .classList.remove('player--active');
+    } else {
+      swithGame();
+    }
+  }
+});
 
 // btn_new.addEventListener('click', initi);
