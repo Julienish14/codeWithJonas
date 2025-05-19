794441571ff16d6e05eff2b6215eb94e444f489c
julien14 <juxishimwe@gmail.com>
2025-05-19 22:47:50 +0200

    re put app again

--- 09-Data-Structures-Operators/starter/challenges.js
+++ 09-Data-Structures-Operators/starter/challenges.js
@@ -107,3 +107,29 @@ team1 < team2 && console.log('Team one is more likely to win');
 team1 > team2 && console.log('Team Two is more likely to win');
 
 */
+
+//2.4
+//Below are two variables called bookTitle and bookAuthor. Reassign them with the values of the title and author properties of the first book object from the books array.
+
+let bookTitle = 'unknown';
+let bookAuthor = 'unknown';
+
+({ title: bookTitle, author: bookAuthor } = books[0]);
+
+console.log(title, author);
+*/
+//2.5
+/* Each book object has a deeply nested rating property as illustrated below:
+{
+    title: 'Algorithms',
+    ...
+    thirdParty: {
+      goodreads: {
+        rating: 4.41,              // <-- HERE
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13
+      }
+    }
+  },
