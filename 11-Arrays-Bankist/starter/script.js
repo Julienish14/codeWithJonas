ce5b3eadc02741b67558fdd0a614fdac2dc383a4
julien14 <juxishimwe@gmail.com>
2025-05-26 12:51:35 +0200

    bank list function arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,8 +71,6 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-containerApp.style.opacity = 0;
-
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
