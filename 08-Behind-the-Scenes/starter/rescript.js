21637c49cc8d7a7d386167ef88c3525c8903c644
julien14 <juxishimwe@gmail.com>
2025-06-12 18:32:50 +0200

    working on js & structu

---
+++ 08-Behind-the-Scenes/starter/rescript.js
@@ -0,0 +1,25 @@
+function calcAge(birthYear) {
+  const age = 2025 - birthYear;
+
+  // console.log(firstName); //here what happens is called variable lookup, the variable was not in this scope, it loops up in scope chain and find it in parent scope
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true; //Var is function scope not like let&const(ES6)
+
+      //Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
+
+      //Reassigning outer scope's variable
+      //output = 'NEW OUTPUT!';
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
