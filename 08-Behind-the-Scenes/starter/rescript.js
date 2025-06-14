743adb8419d6e9593b634eb01c1341e5c14a52b2
julien14 <juxishimwe@gmail.com>
2025-06-14 18:32:05 +0200

    working on js & structu

--- 08-Behind-the-Scenes/starter/rescript.js
+++ 08-Behind-the-Scenes/starter/rescript.js
@@ -84,3 +84,24 @@ const calcAgeArrow = birthYear => {
   //   console.log(this); //arrow function doesn't get it own this keyword
 };
 calcAgeArrow();
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2024 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge; //Method borrowing
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
+
+// var firstName = 'Matilda';
