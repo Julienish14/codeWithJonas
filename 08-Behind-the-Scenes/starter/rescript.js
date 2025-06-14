08bf355557fbdf1eb294f7ff7ef8cb43765502d6
julien14 <juxishimwe@gmail.com>
2025-06-14 18:34:11 +0200

    working on js & structu

--- 08-Behind-the-Scenes/starter/rescript.js
+++ 08-Behind-the-Scenes/starter/rescript.js
@@ -122,3 +122,14 @@ addExpr(2, 5, 6, 71);
 // }; //argument function doesn't exist in arrow fun
 // addArrow(2, 5, 6);
 */
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
