f0244e7defda14c5687972f4f15251d4026a5e69
julien14 <juxishimwe@gmail.com>
2025-06-16 10:27:26 +0200

    working on js & structu

--- 08-Behind-the-Scenes/starter/rescript.js
+++ 08-Behind-the-Scenes/starter/rescript.js
@@ -146,3 +146,20 @@ const jessica = {
   lastName: 'Williams',
   age: 27,
 };
+
+// compying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage: ', jessica2);
+console.log('After marriage: ', jessicaCopy);
