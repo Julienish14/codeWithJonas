4084cdd9452a96769999a3a9ef95e67d7fc1664d
julien14 <juxishimwe@gmail.com>
2025-06-16 12:21:41 +0200

    work

--- 08-Behind-the-Scenes/starter/rescript.js
+++ 08-Behind-the-Scenes/starter/rescript.js
@@ -163,12 +163,12 @@ jessicaCopy.family.push('John');
 console.log('Before marriage: ', jessica2);
 console.log('After marriage: ', jessicaCopy);
 //Solution 1
-const self = this; // self or that
-const isMelleial = function () {
-  console.log(self);
-  console.log(self.year >= 1981 && self.year <= 1996);
-  //   console.log(this.year >= 1981 && this.year <= 1996);
-};
+// const self = this; // self or that
+// const isMellenial = function () {
+//   console.log(self);
+//   console.log(self.year >= 1981 && self.year <= 1996);
+//   //   console.log(this.year >= 1981 && this.year <= 1996);
+// };
 
 //Solution 2
 const isMellenial = () => {
