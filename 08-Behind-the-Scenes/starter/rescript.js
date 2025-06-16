394f534b2e97e393427e656bb8839f8774df0145
julien14 <juxishimwe@gmail.com>
2025-06-16 12:19:03 +0200

    work

--- 08-Behind-the-Scenes/starter/rescript.js
+++ 08-Behind-the-Scenes/starter/rescript.js
@@ -162,3 +162,17 @@ jessicaCopy.family.push('John');
 
 console.log('Before marriage: ', jessica2);
 console.log('After marriage: ', jessicaCopy);
+//Solution 1
+// const self = this; // self or that
+// const isMellenial = function () {
+//   console.log(self);
+//   console.log(self.year >= 1981 && self.year <= 1996);
+//   //   console.log(this.year >= 1981 && this.year <= 1996);
+// };
+
+//Solution 2
+const isMellenial = () => {
+  //Arrow func use this keyword from its parent
+  console.log(this);
+  console.log(this.year >= 1981 && this.year <= 1996);
+};
