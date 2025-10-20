43c8b9d41b8fd85c0da7d2ee0fed8f5e6a326026
julien14 <juxishimwe@gmail.com>
2025-10-20 23:13:38 +0200

    r

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,7 +4,6 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-// const modalT = document.querySelector('.modal-title');
 
 const closeModal = function () {
   modal.classList.add('hidden');
